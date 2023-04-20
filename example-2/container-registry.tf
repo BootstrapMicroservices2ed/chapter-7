4ea6680c0314ee6ed05e323e942eee9f177e9edd
Kwee Tan <kwee@eaglecapsoftware.com>
2023-04-19 20:44:58 -0700

    ch7 work localized

--- example-2/container-registry.tf
+++ example-2/container-registry.tf
@@ -4,7 +4,7 @@
 resource "azurerm_container_registry" "container_registry" {
   name                = "flixtube"
   resource_group_name = azurerm_resource_group.flixtube.name
-  location            = "eastus"
+  location            = "westus"
   admin_enabled       = true
   sku                 = "Basic"
 }
